ec5caedc89463a90e1ea3ef09e22e0e9fe048347
leooportilla <leoportilla67@gmail.com>
2023-03-30 11:33:20 -0400

    Mejora en el menu Agregue enlaces de mi redes

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,6 +1,7 @@
 const Opcion = document.querySelector(`.header__enlace`)
-const Icono  = document.querySelector(`.header__icon-image`)
-const Boton  = document.querySelector(`.header__switchbutton-label`)
+const Icono = document.querySelector(`.header__icon-image`)
+const Boton = document.querySelector(`.header__switchbutton-label`)
+const Main = document.querySelector(`main`)
 
 export function Menu() {
 
@@ -14,14 +15,18 @@ export function Menu() {
             //! If para poder cambiar los iconos en torno al modo oscuro
             if (Opcion.classList.contains(`open`)) {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/close_dark.png`) : Icono.setAttribute(`src`, `./media/images/close_light.png`)
-               
+                Main.style.height = `100vh`
+
             } else {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
+                Main.style.height = ``
             }
         }
 
         //! Si se selecciona una opcion, cerrar el menu inmediato
         if (evento.target.matches(`.header__enlace-a`)) {
+            evento.preventDefault
+            Main.style.height = `100%`
             Opcion.classList.remove(`open`)
             Boton.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
         }
