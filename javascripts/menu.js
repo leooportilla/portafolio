cbc1aa043ebbe7bd7d40448e2144abf91b3a2774
leooportilla <leoportilla67@gmail.com>
2023-04-19 11:23:14 -0400

    Cambiando la manera en la que abre el menu

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,8 +1,6 @@
 const Opcion = document.querySelector(`.header__enlace`)
 const Icono = document.querySelector(`.header__icon-image`)
 const Boton = document.querySelector(`.header__switchbutton-label`)
-const Main = document.querySelector(`main`)
-const Redes = document.querySelector(`.headline`)
 
 export function Menu() {
 
@@ -16,19 +14,14 @@ export function Menu() {
             //! If para poder cambiar los iconos en torno al modo oscuro
             if (Opcion.classList.contains(`open`)) {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/close_dark.png`) : Icono.setAttribute(`src`, `./media/images/close_light.png`)
-                Main.style.height = `100vh`
-                Redes.style.display = `flex`
 
             } else {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
-                Main.style.height = ``
-                Redes.style.display = `none`
             }
         }
 
         //! Si se selecciona una opcion, cerrar el menu inmediato
         if (evento.target.matches(`.links`)) {
-            Main.style.height = ``
             Opcion.classList.remove(`open`)
             Boton.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
         }
