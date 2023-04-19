e5f2a321fd93abe88d2915e817ed4666750d4a06
leooportilla <leoportilla67@gmail.com>
2023-04-19 11:29:54 -0400

    Correciones en el menu

--- javascripts/menu.js
+++ javascripts/menu.js
@@ -1,6 +1,8 @@
 const Opcion = document.querySelector(`.header__enlace`)
 const Icono = document.querySelector(`.header__icon-image`)
 const Boton = document.querySelector(`.header__switchbutton-label`)
+const Main = document.querySelector(`main`)
+const Redes = document.querySelector(`.headline`)
 
 export function Menu() {
 
@@ -14,14 +16,17 @@ export function Menu() {
             //! If para poder cambiar los iconos en torno al modo oscuro
             if (Opcion.classList.contains(`open`)) {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/close_dark.png`) : Icono.setAttribute(`src`, `./media/images/close_light.png`)
+                Main.style.height = `90vh` 
 
             } else {
                 Boton.classList.contains(`active`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
+                Main.style.height = ``
             }
         }
 
         //! Si se selecciona una opcion, cerrar el menu inmediato
         if (evento.target.matches(`.links`)) {
+            Main.style.height = ``
             Opcion.classList.remove(`open`)
             Boton.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/menu_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
         }
