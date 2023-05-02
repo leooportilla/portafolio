1529dddd68cbf363c0343be984b6740eadccebc0
leooportilla <leoportilla67@gmail.com>
2023-05-02 09:45:59 -0400

    Optimizacion al JavaScript

--- javascripts/button.js
+++ javascripts/button.js
@@ -21,92 +21,62 @@ let SubIcon
 let Process
 let Description
 
+function setImage(Modo, OneColor, TwoColor, ThreeColor) {
+    Sun.setAttribute("src", `./media/images/sun_${Modo}.png`)
+    Moon.setAttribute("src", `./media/images/moon_${Modo}.png`)
+    Flecha.setAttribute("src", `./media/images/slide_${Modo}.png`)
+    Instagram.setAttribute("src", `./media/images/instagram_${Modo}.png`)
+    Linkedin.setAttribute("src", `./media/images/linkedin_${Modo}.png`)
+    GitHud.setAttribute("src", `./media/images/githud_${Modo}.png`)
+    Search.setAttribute("src", `./media/images/search_${Modo}.png`)
+    Followers.setAttribute("src", `./media/images/followers_${Modo}.png`)
+    Following.setAttribute("src", `./media/images/following_${Modo}.png`)
+    Repository.setAttribute("src", `./media/images/repository_${Modo}.png`)
+
+    Body.style.backgroundColor = `${OneColor}`
+    SubBody.style.backgroundColor = `${OneColor}`
+    InputText.style.backgroundColor = `${TwoColor}`
+    InputText.style.color = `${OneColor}`
+    Summary.style.color = `${ThreeColor}`
+
+    //! Todos las etiquetas que contengan el atributo data-dark cambian el color con el id
+    Dark.forEach(elemento => elemento.setAttribute(`id`, `${Modo}`))
+
+    //! Todas los iconos de los enlaces a las redes sociales cambia de modo
+    Arrow.forEach(elemento => elemento.setAttribute(`src`, `./media/images/arrow_${Modo}.png`))
+
+    //! Evalua si el panel de informacion del lenguaje esta abierto para poder cambiar al modo
+    if (SubBody.classList.contains(`name`)) {
+        SubIcon = document.querySelector(`.name__icon`)
+        Process = document.querySelector(`.name__process`)
+        Description = document.querySelector(`.name__summary`)
+
+        SubIcon.setAttribute(`src`, `./media/images/close_${Modo}.png`)
+        Description.style.color = `${ThreeColor}`
+        Process.style.color = `${ThreeColor}`
+    }
+
+    //! Cambia los iconos del menu segun el modo
+    Enlace.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/close_${Modo}.png`) : Icono.setAttribute(`src`, `./media/images/menu_${Modo}.png`)
+}
+
+//* Cambiar iconos al modo light, colores de fondo y texto
+function Light() {
+    setImage(`dark`, `#ffffff`, `#232528`, `#232528`)
+}
+
+//* Cambio el modo a light con esta funcion para poder agregarle la clase active
+function MoodLight() {
+    Buttoon.classList.toggle(`active`)
+    Light()
+}
+
+//* Cambiar iconos al modo dark, colores de fondo y texto
+function MoodDark() {
+    setImage(`light`, `#232528`, `#ffffff`, `#bebebe`)
+}
+
 export function Button() {
-
-    //* Cambiar iconos al modo light, colores de fondo y texto
-    function Light() {
-        Sun.setAttribute("src", `./media/images/sun_dark.png`)
-        Moon.setAttribute("src", `./media/images/moon_dark.png`)
-        Flecha.setAttribute("src", `./media/images/slide_dark.png`)
-        Instagram.setAttribute("src", `./media/images/instagram_dark.png`)
-        Linkedin.setAttribute("src", `./media/images/linkedin_dark.png`)
-        GitHud.setAttribute("src", `./media/images/githud_dark.png`)
-        Search.setAttribute("src", `./media/images/search_dark.png`)
-        Followers.setAttribute("src", `./media/images/followers_dark.png`)
-        Following.setAttribute("src", `./media/images/following_dark.png`)
-        Repository.setAttribute("src", `./media/images/repository_dark.png`)
-
-        Body.style.backgroundColor = `#ffffff`
-        SubBody.style.backgroundColor = `#ffffff`
-        InputText.style.backgroundColor = `#232528`
-        InputText.style.color = `#ffffff`
-        Summary.style.color = `#232528`
-
-        //! Todos las etiquetas que contengan el atributo data-dark cambian el color con el id
-        Dark.forEach(elemento => elemento.setAttribute(`id`, `dark`))
-
-        //! Todas los iconos de los enlaces a las redes sociales cambia de modo
-        Arrow.forEach(elemento => elemento.setAttribute(`src`, `./media/images/arrow_dark.png`))
-
-        //! Evalua si el panel de informacion del lenguaje esta abierto para poder cambiar al modo 
-        if (SubBody.classList.contains(`name`)) {
-            SubIcon = document.querySelector(`.name__icon`)
-            Process = document.querySelector(`.name__process`)
-            Description = document.querySelector(`.name__summary`)
-
-            SubIcon.setAttribute(`src`, "./media/images/close_dark.png")
-            Description.style.color = `#232528`
-            Process.style.color = `#232528`
-        }
-
-        //! Cambia los iconos del menu segun el modo
-        Enlace.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/close_dark.png`) : Icono.setAttribute(`src`, `./media/images/menu_dark.png`)
-    }
-
-    //* Cambio el modo a light con esta funcion para poder agregarle la clase active
-    function MoodLight() {
-        Buttoon.classList.toggle(`active`)
-        Light()
-    }
-
-    //* Cambiar iconos al modo dark, colores de fondo y texto
-    function MoodDark() {
-        Sun.setAttribute("src", `./media/images/sun_light.png`)
-        Moon.setAttribute("src", `./media/images/moon_light.png`)
-        Flecha.setAttribute("src", `./media/images/slide_light.png`)
-        Instagram.setAttribute("src", `./media/images/instagram_light.png`)
-        Linkedin.setAttribute("src", `./media/images/linkedin_light.png`)
-        GitHud.setAttribute("src", `./media/images/githud_light.png`)
-        Search.setAttribute("src", `./media/images/search_light.png`)
-        Followers.setAttribute("src", `./media/images/followers_light.png`)
-        Following.setAttribute("src", `./media/images/following_light.png`)
-        Repository.setAttribute("src", `./media/images/repository_light.png`)
-
-        Body.style.backgroundColor = `#232528`
-        SubBody.style.backgroundColor = `#232528`
-        InputText.style.backgroundColor = `#ffffff`
-        InputText.style.color = `#232528`
-        Summary.style.color = `#bebebe`
-
-        //! Todos las etiquetas que contengan el atributo data-dark cambian el color con el id
-        Dark.forEach(elemento => elemento.setAttribute(`id`, ``))
-
-        //! Evalua si el panel de informacion del lenguaje esta abierto para poder cambiar al modo 
-        Arrow.forEach(elemento => elemento.setAttribute(`src`, `./media/images/arrow_light.png`))
-
-        //! Evalua si el panel de informacion del lenguaje esta abierto para poder cambiar al modo 
-        if (SubBody.classList.contains(`name`)) {
-            SubIcon = document.querySelector(`.name__icon`)
-            Process = document.querySelector(`.name__process`)
-            Description = document.querySelector(`.name__summary`)
-
-            SubIcon.setAttribute(`src`, "./media/images/close_light.png")
-            Description.style.color = `#bebebe`
-            Process.style.color = `#bebebe`
-        }
-        //! Cambia los iconos del menu segun el modo
-        Enlace.classList.contains(`open`) ? Icono.setAttribute(`src`, `./media/images/close_light.png`) : Icono.setAttribute(`src`, `./media/images/menu_light.png`)
-    }
 
     //* Al clickear el boton cambia de posicion con la clase active
     document.addEventListener(`click`, evento => {
