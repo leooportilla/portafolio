3e837e41a7efc805a77a03dc3b3a624836807088
leooportilla <leoportilla67@gmail.com>
2023-07-18 11:34:54 -0400

    Mood Home Correciones en las variables y agregue algunos comentarios

--- scripts/modules/button_mode.js
+++ scripts/modules/button_mode.js
@@ -1,10 +1,12 @@
 const buttonSwitch = document.querySelector(`.switch`)
 const labelSwitch = document.querySelector(`.switch__label`)
+const documentHtml = document.querySelector(`html`)
 
 export const buttonMode = () => {
 
     //! Agregamos un evento al Boton de cambiar el modo al momento que ocurrar un click quitamos o agregamos la clase Move
     buttonSwitch.addEventListener(`click`, () => {
         labelSwitch.classList.toggle(`move`)
+        documentHtml.classList.toggle(`dark`)
     })
 }
--- scripts/public/main.js
+++ scripts/public/main.js
@@ -2,12 +2,14 @@
 
 const buttonSwitch = document.querySelector(`.switch`);
 const labelSwitch = document.querySelector(`.switch__label`);
+const documentHtml = document.querySelector(`html`);
 
 const buttonMode = () => {
 
     //! Agregamos un evento al Boton de cambiar el modo al momento que ocurrar un click quitamos o agregamos la clase Move
     buttonSwitch.addEventListener(`click`, () => {
         labelSwitch.classList.toggle(`move`);
+        documentHtml.classList.toggle(`dark`);
     });
 };
 
