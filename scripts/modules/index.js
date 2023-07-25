2dfc80ca4022add3a6f4dabf502668b4e6879867
leooportilla <leoportilla67@gmail.com>
2023-07-25 19:03:30 -0400

    Interactividad al boton de inicio

--- scripts/modules/index.js
+++ scripts/modules/index.js
@@ -1,4 +1,5 @@
 import { buttonMode } from "./button_mode";
+import { buttonTop } from "./button_top";
 import { profile, search } from "./profile";
 import { project } from "./project";
 import { skillsInformation } from "./skills_information";
@@ -6,6 +7,7 @@ import { skillsInformation } from "./skills_information";
 document.addEventListener(`DOMContentLoaded`, () => {
     profile()
     project()
+    buttonTop()
 })
 
 search()
--- scripts/public/main.js
+++ scripts/public/main.js
@@ -13,6 +13,29 @@ const buttonMode = () => {
     });
 };
 
+//! Declaracion de variable para el boton de subir al inicio
+const headerTop    = document.querySelector(`.header`);
+const buttonHome   = document.querySelector(`.buttonhome`);
+const sectionGuide = document.querySelector(`.home__container`);
+
+//! Estaremos al pendiente al momento que el scroll pase una seccion de la pagina para que el boton pueda aparecer
+const buttonTop = () => {
+
+    //! Evento del scroll para que el navegador este pendiente
+    document.addEventListener(`scroll`, () =>   {
+
+        //! Condicion si llega a pasar la  seccion agregamos la clase al boton
+        if (window.scrollY > sectionGuide.getBoundingClientRect().top + window.scrollY) buttonHome.classList.add(`active-button`);
+        
+        //! Condicion por si llega a subir de nuevo el usuario al inicio, aqui quitamos la clase al boton
+        if (window.scrollY <= headerTop.getBoundingClientRect().top + window.scrollY) buttonHome.classList.remove(`active-button`);
+    
+    });
+
+    //! Evento click al boton para que pueda mandar al incio al usuario
+    buttonHome.addEventListener(`click`, () => window.scrollTo({behavior: `smooth`, top: 0}));
+};
+
 const userName        = document.querySelector(`.container__user-information-name`);
 const userImage       = document.querySelector(`.container__image`);
 const userGitHud      = document.querySelector(`.container__user-information-link`);
@@ -870,6 +893,7 @@ const skillsInformation = () => {
 document.addEventListener(`DOMContentLoaded`, () => {
     profile();
     project$1();
+    buttonTop();
 });
 
 search();
